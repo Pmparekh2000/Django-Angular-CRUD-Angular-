3a999927caf5a0dfc0951d1109200ce3d7d073a4
Pmparekh2000 <pmp.tech2000@gmail.com>
2020-09-20 22:50:34 +0530

    Completed CRUD operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,11 @@ import { ApiService } from './api.service';
 })
 export class AppComponent {
   movies = [{title: 'test'}];
+  selectedMovie;
 
   constructor(private api: ApiService) {
     this.getMovies();
+    this.selectedMovie = {id: -1, title: '', desc: '', year: 0};
   }
 
   getMovies = () => {
@@ -23,5 +25,72 @@ export class AppComponent {
         console.log(error);
       }
     );
+  }
+
+  movieClicked = (movie) => {
+    // console.log(movie.id);
+    // console.log(movie.title);
+    this.api.getOneMovies(movie.id).subscribe(
+      data => {
+        this.selectedMovie = data;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  updateMovie = () => {
+    this.api.updateMovie(this.selectedMovie).subscribe(
+      data => {
+        this.selectedMovie = data;
+        this.getMovies();
+        // this.movies = data;
+        // console.log(this.selectedMovie);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+
+    this.selectedMovie.title = "";
+    this.selectedMovie.desc = "";
+    this.selectedMovie.year = "";
+  }
+
+  createMovie = () => {
+    this.api.createMovie(this.selectedMovie).subscribe(
+      data => {
+        this.movies.push(data);
+        // this.movies = data;
+        // console.log(this.selectedMovie);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+
+    this.selectedMovie.title = "";
+    this.selectedMovie.desc = "";
+    this.selectedMovie.year = "";
+  }
+
+  deleteMovie = () => {
+    this.api.deleteMovie(this.selectedMovie.id).subscribe(
+      data => {
+        this.getMovies();
+        // this.movies.push(data);
+        // this.movies = data;
+        // console.log(this.selectedMovie);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+
+    // this.title = "";
+    this.selectedMovie.title = "";
+    this.selectedMovie.desc = "";
+    this.selectedMovie.year = "";
   }
 }
